ef0dea05fd406fcd116701258cd8aa4e02e71869
darkosimic025 <darkosimic025@gmail.com>
2022-01-26 16:52:04 +0100

    Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Expenses from "./components/Expenses";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const expenses = [
+    { title: "Car Insurance", amount: 295.54, date: new Date(2022, 1, 26) },
+    { title: "Car Insurance", amount: 400.33, date: new Date(2022, 1, 26) },
+    { title: "Feed the dog", amount: 960.66, date: new Date(2022, 2, 26) },
+    { title: "Car Insurance", amount: 295.54, date: new Date(2022, 1, 26) },
+  ];
+
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let`s get started!"),
+    React.createElement(Expenses, { items: expenses })
   );
+
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items={expenses}/>
+  //   </div>
+  // );
 }
 
 export default App;
